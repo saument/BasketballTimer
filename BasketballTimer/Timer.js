dbceef9f90a1e8eb31c446640233fd4eec1b09b3
saument <lifeguard325@yahoo.com>
2014-04-13 23:24:44 -0400

    javascript object

--- BasketballTimer/Timer.js
+++ BasketballTimer/Timer.js
@@ -2,67 +2,82 @@
  * Created by Stephen
  */
 
-//This var will store the interval used to decrement the clock.
-//It will be referenced in both start and stop
-var runningClock;
 
-//This var prevents the user from "stacking" function calls
-var started = false;
+(function() {
 
-function start () {
-    //If the clock is already started there is no need to take any action
-    if(!started){
-        //This user should not be able to edit the clock after it has started
-        document.getElementById("gameMinutes").setAttribute("disabled", "disabled");
-        document.getElementById("gameSeconds").setAttribute("disabled", "disabled");
-        document.getElementById("gameTenths").setAttribute("disabled", "disabled");
-
-        //set the interval to one second
-        runningClock=setInterval(function(){clock()}, 1000);
-        started = true;
+    window.Clock = function (clk){
+        this.minutes = clk + "Minutes";
+        this.seconds = clk + "Seconds";
+        this.tenths = clk + "Tenths";
+        return this;
     }
-}
 
-function stop () {
-    //Stop the clock from running
-    clearInterval(runningClock);
+    //This var will store the interval used to decrement the clock.
+    //It will be referenced in both start and stop
+    this.runningClock;
 
-    //This user should not be able to edit the clock even if it is stopped
-    document.getElementById("gameMinutes").setAttribute("disabled", "disabled");
-    document.getElementById("gameSeconds").setAttribute("disabled", "disabled");
-    document.getElementById("gameTenths").setAttribute("disabled", "disabled");
-    started = false;
-}
+    //This var prevents the user from "stacking" function calls
+    this.started = false;
 
-function edit () {
-    //While editing the clock there is no need for it to be running.
-    stop();
+    Clock.prototype.start = function() {
+        //If the clock is already started there is no need to take any action
+        if(!this.started){
+            //This user should not be able to edit the clock after it has started
+            document.getElementById(this.minutes).setAttribute("disabled", "disabled");
+            document.getElementById(this.seconds).setAttribute("disabled", "disabled");
+            document.getElementById(this.tenths).setAttribute("disabled", "disabled");
 
-    //Enable the fields so the user can edit them
-    document.getElementById("gameMinutes").removeAttribute("disabled");
-    document.getElementById("gameSeconds").removeAttribute("disabled");
-
-    //The user will not be allowed to edit the tenths of seconds
-    //Set the value to 0
-    document.getElementById("gameTenths").setAttribute("disabled", "disabled");
-    document.getElementById("gameTenths").value = 0;
-
-    //Since we stopped the clock it is safe to set the flag back to false
-    started = false;
-}
-
-function clock () {
-    if(document.getElementById("gameSeconds").value > 0){
-        document.getElementById("gameSeconds").value -= 1;
-    }else{
-        if(document.getElementById("gameMinutes").value > 0){
-            document.getElementById("gameMinutes").value -= 1;
-            document.getElementById("gameSeconds").value = 59;
-        }else{
-
+            //set the interval to one second
+            this.runningClock = setInterval(function(){this.tick()}, 1000);
+            this.started = true;
         }
     }
-}
+
+    Clock.prototype.stop = function() {
+        //Stop the clock from running
+        clearInterval(this.runningClock);
+
+        //This user should not be able to edit the clock even if it is stopped
+        document.getElementById(this.minutes).setAttribute("disabled", "disabled");
+        document.getElementById(this.seconds).setAttribute("disabled", "disabled");
+        document.getElementById(this.tenths).setAttribute("disabled", "disabled");
+        this.started = false;
+    }
+
+    Clock.prototype.edit = function() {
+        //While editing the clock there is no need for it to be running.
+        this.stop();
+
+        //Enable the fields so the user can edit them
+        document.getElementById(this.minutes).removeAttribute("disabled");
+        document.getElementById(this.seconds).removeAttribute("disabled");
+
+        //The user will not be allowed to edit the tenths of seconds
+        //Set the value to 0
+        document.getElementById(this.tenths).setAttribute("disabled", "disabled");
+        document.getElementById(this.tenths).value = 0;
+
+        //Since we stopped the clock it is safe to set the flag back to false
+        this.started = false;
+    }
+
+    Clock.prototype.tick = function () {
+        if(document.getElementById(this.seconds).value > 0){
+            document.getElementById(this.seconds).value -= 1;
+        }else{
+            if(document.getElementById(this.minutes).value > 0){
+                document.getElementById(this.minutes).value -= 1;
+                document.getElementById(this.minutes).value = 59;
+            }else{
+
+            }
+        }
+    }
+}());
+
+var gameClock = new Clock("game");
+var shotClock = new Clock("shot");
+
 
 //This function is called when the user wants to modify the score of either team
 //The parameters specify the team and the score adjustment ( + or - )
